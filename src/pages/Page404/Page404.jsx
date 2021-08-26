c81915fbeb684760e9bfa51b88c5b9058af9bd51
reddvil <zurab.okroshidze.1@btu.edu.ge>
2021-08-26 17:01:16 +0300

    added 404 page

---
+++ src/pages/Page404/Page404.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Page404() {
+  return (
+    <div
+      style={{
+        marginTop: '30vh',
+        marginBottom: 150,
+        textAlign: 'center',
+        fontSize: 36,
+        fontFamily: 'FiraGo-Medium',
+      }}>
+      Page Not Found
+    </div>
+  );
+}
+
+export default Page404;
